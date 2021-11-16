36cb958b30fd58bd591d527e011e5f59e58b483d
levantong <vantong112.vpb@gmail.com>
2021-11-16 20:23:14 +0700

    commit to main

--- js/hide_navbar.js
+++ js/hide_navbar.js
@@ -7,13 +7,6 @@ function scrollFunction() {
     document.querySelector(".navbar-brand").style.display = "none"
     document.querySelector(".nav-call button").style.padding = "0.6em 1.5em";
     document.getElementById("navbar").style.boxShadow = "0 0 10px 0 rgba(204, 204, 204, 0.5)";
-
-
-    // document.getElementById("navbar").style.height = "none";
-  // else if (document.body.scrollTop > 580 || document.documentElement.scrollTop > 580) {
-  //   document.getElementById("navbar").style.display = "block";
-
-
   } else{
       document.getElementById("navbar").style.background = "transparent";
     document.getElementById("navbar").style.height = "96px"
